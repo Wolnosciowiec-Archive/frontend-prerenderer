2929faa48b80b3f998fafd1ec71f66b70ef86b39
Free Man <admin@wolnosciowiec.org>
2018-04-20 06:53:25 +0200

    Added migrations to be run also.

--- docker/common/entrypoint-prerenderer.sh
+++ docker/common/entrypoint-prerenderer.sh
@@ -4,7 +4,7 @@ chown node:node /www/frontend-prerenderer/data -R
 
 # if the container has files mounted as a volume, then probably the composer and npm was not run yet
 if [[ ! -d /www/frontend-prerenderer/vendor ]] || [[ ! -d /www/frontend-prerenderer/node_modules ]]; then
-    cd /www/frontend-prerenderer && make deploy
+    cd /www/frontend-prerenderer && make deploy migrate
 fi
 
 exec /entrypoint-php-node.sh
