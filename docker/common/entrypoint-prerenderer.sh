76a7f9f8767a47fc0e9032c24c7277876dc31343
Free Man <admin@wolnosciowiec.org>
2018-04-20 06:51:54 +0200

    Fixes to the Docker deployment. The npm and composer should now be run.

--- docker/common/entrypoint-prerenderer.sh
+++ docker/common/entrypoint-prerenderer.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 chown node:node /www/frontend-prerenderer/data -R
+
+# if the container has files mounted as a volume, then probably the composer and npm was not run yet
+if [[ ! -d /www/frontend-prerenderer/vendor ]] || [[ ! -d /www/frontend-prerenderer/node_modules ]]; then
+    cd /www/frontend-prerenderer && make deploy
+fi
+
 exec /entrypoint-php-node.sh
